41c2ce5e4657d6efbc2cf79260fe898ab4e95b2f
Jack Middlebrook <jack@creativehike.com>
2024-07-28 15:41:13 +0800

    update on resize

--- src/components/data-viz/RandomContours.svelte
+++ src/components/data-viz/RandomContours.svelte
@@ -1,18 +1,35 @@
 <script lang="ts">
-  import {
-    contours as countoursD3,
-    type ContourMultiPolygon,
-  } from 'd3-contour';
+  import { contours as countoursD3 } from 'd3-contour';
   import { select } from 'd3-selection';
   import { range } from 'd3-array';
   import { geoPath, geoIdentity } from 'd3-geo';
+  import { onMount } from 'svelte';
 
-  function updateSvg(
-    contourData: ContourMultiPolygon[],
-    width: number,
-    height: number,
-    n: number,
-  ) {
+  function debounce(func: Function, wait: number) {
+    let timeout: number;
+    return function (...args: any[]) {
+      const later = () => {
+        clearTimeout(timeout);
+        func(...args);
+      };
+      clearTimeout(timeout);
+      // @ts-ignore error
+      timeout = setTimeout(later, wait);
+    };
+  }
+
+  function updateSvg(width: number, height: number) {
+    // scale n with the size of the window 50 on small windows, then progressively more
+    const n = Math.max(50, Math.floor(Math.min(width, height) / 10));
+    const values = new Array(n * n).fill(0).map(() => Math.random());
+
+    // Create the contour generator
+    const contours = countoursD3()
+      .size([n, n])
+      .thresholds(range(0, 1, 0.1));
+
+    // Generate contour data
+    const contourData = contours(values);
     const scale = Math.max(width, height) / n;
     const projection = geoIdentity().scale(scale).translate([0, 0]);
 
@@ -37,21 +54,16 @@
   let width = 0;
   let height = 0;
 
-  const n = 50;
-  const values = new Array(n * n).fill(0).map(() => Math.random());
+  onMount(() => {
+    updateSvg(width, height);
+  });
 
-  // Create the contour generator
-  const contours = countoursD3()
-    .size([n, n])
-    .thresholds(range(0, 1, 0.1));
-
-  // Generate contour data
-  const contourData = contours(values);
-
-  $: if (width && height) {
-    updateSvg(contourData, width, height, n);
-  }
+  const debouncedUpdateSvg = debounce(() => updateSvg(width, height), 100);
 </script>
 
-<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+<svelte:window
+  bind:innerWidth={width}
+  bind:innerHeight={height}
+  on:resize={debouncedUpdateSvg}
+/>
 <svg class="absolute left-0 top-0" {width} {height} />
